bb45ab5eea27f79d76f31b571eee13378b39c1f8
Lwooles <43282868+Lwooles@users.noreply.github.com>
2018-11-17 14:46:41 +1300

    changing comment on new branch

--- MyFirstProject/src/HelloWorldExample.java
+++ MyFirstProject/src/HelloWorldExample.java
@@ -2,7 +2,7 @@
 public class HelloWorldExample {
 
 	public static void main(String[] args) {
-		// Output Hello World to the user
+		// TODO trying something new
 		System.out.println("Hello World");
 	}
 
